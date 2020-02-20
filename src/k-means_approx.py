680e4a2ffc46083ec1aca74fad01a7f398ee8f5e
GabrieleNardo <gabriele.nardo9@gmail.com>
2020-02-20 23:24:03 +0100

    Fix empty cluster issue; reading monodimensional daset

--- src/k-means_approx.py
+++ src/k-means_approx.py
@@ -8,6 +8,7 @@
 #A CSV file containig all the point to process is used as input
 
 import random 
+import csv
 import pandas as pand
 import numpy as nump
 import matplotlib.pyplot as pyplt
@@ -46,11 +47,10 @@ class K_Means_Alg: 	#class K-Means_Alg define attributes and methods to perform 
 				self.clusters[pointclass].append(point) #append each element to its cluster (clusters[pointclass] where pointclass is the index that identify the cluster)
 
 			previous = dict(self.centroids) #store centroids to compare them later to new re-calculated centroids
-
 			#average the cluster datapoints to re-calculate the centroids
 			for pointclass in self.clusters:
-				self.centroids[pointclass] = nump.average(self.clusters[pointclass], axis = 0) #centroids[] now contains the coordinates of the new recalculated centroids
-			
+				if self.clusters[pointclass]: self.centroids[pointclass] = nump.average(self.clusters[pointclass], axis = 0) #centroids[] now contains the coordinates of the new recalculated centroids. The if instruction verify that the cluster is not empty. If cluster is empty, centroid don't change coordinates
+
 			end = True #end is used to verify if the algorithm has converged
 			print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
 			print("\nIteration: " +str(it+1))
@@ -72,9 +72,14 @@ class K_Means_Alg: 	#class K-Means_Alg define attributes and methods to perform 
 
 def main(path,n=3,approx_error=0.0000, iterations=500): #set arguments(n, approx_error, iterations) to default value if they're no passed
 	
+	with open(path, "r") as f:
+		reader = csv.reader(f)
+		i = next(reader) #i is an array containing the headers of the csv file
 	
 	data = pand.read_csv(path) #read the data source from the path argument
-	data = data[['V1', 'V2']]  #V1 and V2 headers of the 'data' dataframe
+	if (len(i) == 1):  #if i == 1, csv file cointains monodimensional points, so we add V2 column with 0 values to all points
+		data['V2']= 0.00
+	print(data)
 	np_data = data.values #returns a numpy array
 	
 	km = K_Means_Alg(n, approx_error, iterations) #instantiate km of K_Means_Alg class
@@ -84,7 +89,7 @@ def main(path,n=3,approx_error=0.0000, iterations=500): #set arguments(n, approx
 	print("Start Plotting..")
 	colors = 30*["b", "r", "g", "c", "m", "y", "k", "#ff8300", "#777777", "#00ff04",  "#840028", "#45ff88", "#00146e", "#7d9300", "#ff6060", "#ffb600"] #every cluster has its color
 	subplt = pyplt.subplot()
-	pyplt.title('K-Means Clusters Results') 
+	pyplt.title('K-Means Clusters Results - Approx error : ' + str(approx_error)) 
 
 
 	for centroid in km.centroids:
@@ -103,8 +108,8 @@ def main(path,n=3,approx_error=0.0000, iterations=500): #set arguments(n, approx
 	pyplt.show()
 
 if __name__ == "__main__":
-	cluster_size = int(input("Enter the cluster size : "))
-	appr_factor = float(input("Enter the approximation factor 0 < appr_factor < 1 : "))
-	cicles = int(input("Enter the iteration number : "))
+	cluster_size = int(input("Enter cluster size : "))
+	appr_factor = float(input("Enter approximation factor 0 < appr_factor < 1 : "))
+	cicles = int(input("Enter MAX iterations number executable : "))
 	input_file = input("Enter input file name ( .csv ) : ")
 	main(path='../dataset/'+ input_file, n=cluster_size, approx_error=appr_factor, iterations=cicles) #set arguments for main function (path, n, approx_error, iterations)
