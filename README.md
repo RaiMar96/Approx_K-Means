14281f3cb57ea926c22710754ae240af117efd73
RaiMar96 <raitimario.mr@gmail.com>
2020-02-21 21:06:07 +0100

    Commit: Final Commit

--- README.md
+++ README.md
@@ -1,55 +1,90 @@
 # K-Means Clustering
-Tesina finale per relativa al corso : INTERNET OF THINGS BASED SMART SYSTEMS - UniCT. <br>
+
+<!--![simulation result](https://archivio.unict.it/sites/default/files/images/orizzontale-grigio.png)-->
+
+Tesina finale relativa al corso : INTERNET OF THINGS BASED SMART SYSTEMS - UniCT.
+
 Questo Tool implementa il  [K-means clustering alogrith](https://en.wikipedia.org/wiki/K-means_clustering) , in versione precisa o approssimata. Il grado di approssimazione viene introdotta tramite i parametri forniti in ingresso allo script ( approx_factor , max_itartions ). Questo permette di implementare la tecnica di Approximate Computing di loop perforation.
 
 La struttura della repository è la seguente :
+
 - **dataset** : contiene i file _.csv_ usati come sorgente dati per la computazione ( la struttura di tali file sarà descrtitta alla sezione apposita ).
 - **src** : contine il codice sorgente dellìapplicazione _.py_ .
-- **output** : contiene il file _cluster.png_ rappresentante il plot dei risultati della clusterizzazione.
+- **output** : contiene il file _cluster.pdf_ rappresentante il plot dei risultati della clusterizzazione.
 
-Tale tool può essere utilizzato come kernel approssimato in **DNN** che si basano su clusterizzazione dei dati.
+Tale tool può essere utilizzato come kernel approssimato in **DNN** che si basano su clusterizzazione dei dati , per implementazioni in device a risorse limitate.
 
 ## Dipendenze
-Il Tool è stato implementato in _python_ ( si pressupone che siano già installati **python3** e **pip3** in caso contrario [download](https://www.python.org/downloads/)). 
+
+Il Tool è stato implementato in **_python_** ( si pressupone che siano già installati **python3** e **pip3** in caso contrario [download](https://www.python.org/downloads/))
+
 Per una corretta eseczuione è necessario installare i seguenti moduli :
+
 - **pandas**
 - **numpy**
 - **matplotlib**
+- **scipy**
+- **random**
 
 L'installazione delle dipendenze deve essere eseguita tramite i seguenti comandi :
-```
+
+```[shell]
 pip3 install pandas
 pip3 install numpy
 pip3 install matplotlib
+pip3 install scipy
 
 ```
-## Struttura Input File
-I dati di input per la computazione come detto in alto devono essere posizionati nella directory _dataset_ , tali dati devono essere forniti come file .csv  ( a scopo di test vengono fotniti i file _test1.csv_, _test2.csv_ ). Tale file verrà fornito come parametro d'ingresso al momento dell'esecuzione dello script.
 
-Nel caso di file bi-dimensionale (come il file di test proposto), il file deve presentare i seguenti header per le colonne : **V1** , **V2**
-Nel caso di file mono-dimensionale (come il file di test proposto), il file deve presentare i seguenti header per le colonne : **V1** 
+## Struttura Input File
+
+I dati di input per la computazione come detto in alto devono essere posizionati nella directory _dataset_ , tali dati devono essere forniti come file .csv  ( a scopo di test vengono forniti i file _test1.csv_, _test2.csv_ ). Tale file verrà fornito come parametro d'ingresso al momento dell'esecuzione dello script.
+
+Nel caso di file bi-dimensionale (come il file di test proposto), il file deve presentare i seguenti header per le colonne : **V1** , **V2**.
+
+Nel caso di file mono-dimensionale (come il file di test proposto), il file deve presentare i seguenti header per le colonne : **V1** .
+
+Per i casi descritti precedentemente il tool fornisce il pieno supoorto alla stampa dei risultati. Questa non è garantita nel caso di dataset  n-dimensionali (con n > 2), che possono comunque essere processati dal tool . 
+
+La struttura dei file n-dimensionali deve seguire la convenzione per gli header descritta in precedenta : **V1**,..,**Vn**
 
 ## Avvio
+
 Per avviare il tool , lanciare da riga di comando i seguenti (valide sia per bash linux che poweshell windows) comandi all'interno della directory principale :
 
-```
+```[python]
 cd ./src
 python3 k-means_approx.py
 
 ```
+
 A seguito di ciò il programma richiederà l'inserimento dei 3 seguenti valori :
+
 - **Enter cluster size :** richiede di inserire un valore intero che rappresenta la dimensione del cluster.
 - **Enter approximation factor 0 < appr_factor < 1 :**richiede di inserire un valore compreso tra 0 e 1 che rappresenta il fattore di approssimazione.
 - **Enter max iteration number :** richede il valore massimo di iterazioni eseguite dall'algoritmo.
 - **Enter input file name ( .csv ) :** richiede il nome del file.csv di input presente in dataset.
 
 ## Esempio
+
 Esempio di Esecuzione con i seguenti parametri :
+
 - cluster size = **5**
 - approximation factor = **0.002**
 - max iter = **50**
-- input file = **test2.csv**
+- input file = **test2.csv** (dataset a due dimensioni)
 
 il risultato dell'esecuzione è rappresentato dalla seguente immagine :
 
-![simulation result](https://github.com/RaiMar96/IoT_project_2k19-20/blob/master/example/example.png)
+![simulation result](https://github.com/RaiMar96/IoT_project_2k19-20/blob/master/example/example-bi.png)
+
+Esempio di Esecuzione con i seguenti parametri :
+
+- cluster size = **5**
+- approximation factor = **0.002**
+- max iter = **50**
+- input file = **test1.csv** (dataset a una dimensione)
+
+il risultato dell'esecuzione è rappresentato dalla seguente immagine :
+
+![simulation result](https://github.com/RaiMar96/IoT_project_2k19-20/blob/master/example/example-mono.png)
--- src/k-means_approx.py
+++ src/k-means_approx.py
@@ -7,12 +7,14 @@
 
 #A CSV file containig all the point to process is used as input
 
-import random 
+import random
+from scipy.spatial import distance
 import csv
 import pandas as pand
 import numpy as nump
 import matplotlib.pyplot as pyplt
 from matplotlib import style
+import random as rd
 
 style.use('ggplot')
 
@@ -22,7 +24,6 @@ class K_Means_Alg: 	#class K-Means_Alg define attributes and methods to perform 
 		self.iterations = iterations    				#set the number of iterations
 		self.n = n 										#set the number of clusters/centroids
 		
-
 	def exe(self, data):   #exe method perform the algorithm
 
 		self.centroids = {} #initialize dictionary to save centroids coordinates
@@ -38,29 +39,30 @@ class K_Means_Alg: 	#class K-Means_Alg define attributes and methods to perform 
 		for it in range(self.iterations):
 			self.clusters = {} #initialize clusters dictionary
 			for i in range(self.n):
-				self.clusters[i] = [] #initialize an array for every cluster(number of centroids -> n)
+				self.clusters[i] = [] #initialize a list for every cluster(number of centroids -> n)
 
 			#choose the nearest centroid based on the distance from the point
 			for point in data:
-				distances = [nump.linalg.norm(point - self.centroids[centroid]) for centroid in self.centroids] #'distances' store the distances from each elements to the n centroids
-				pointclass = distances.index(min(distances)) #'pointclass' store for each element the index of the cluster based on the min distance from each centroid
-				self.clusters[pointclass].append(point) #append each element to its cluster (clusters[pointclass] where pointclass is the index that identify the cluster)
+				distances = [distance.euclidean(point , self.centroids[centroid]) for centroid in self.centroids] #'distances' store the euclidean distances from the point to each centroids
+				pointclass = distances.index(min(distances)) #'pointclass' store for each point, the index of the cluster based on the min distance from each centroid
+				self.clusters[pointclass].append(point) #append each point to its cluster (clusters[pointclass] where pointclass is the index that identify the cluster)
 
 			previous = dict(self.centroids) #store centroids to compare them later to new re-calculated centroids
+
 			#average the cluster datapoints to re-calculate the centroids
 			for pointclass in self.clusters:
 				if self.clusters[pointclass]: self.centroids[pointclass] = nump.average(self.clusters[pointclass], axis = 0) #centroids[] now contains the coordinates of the new recalculated centroids. The if instruction verify that the cluster is not empty. If cluster is empty, centroid don't change coordinates
 
 			end = True #end is used to verify if the algorithm has converged
 			print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-			print("\nIteration: " +str(it+1))
+			print("\nIteration: " +str(it+1)) #print current iteration
 			print("Normalized coordinates of new centroids:")
-			for centroid in self.centroids:
+			for centroid in self.centroids: #for each centroid, 'original centroid' contain its previous coordinates, whereas 'curr' contain its current coordinates
 				original_centroid = previous[centroid]
 				curr = self.centroids[centroid]
-				print(nump.linalg.norm(nump.sum((curr-original_centroid)/original_centroid)))
+				print(nump.sum(distance.euclidean(curr,original_centroid)/original_centroid))
 
-				if nump.linalg.norm(nump.sum((curr - original_centroid)/original_centroid)) > self.approx_error: #verifying if current centroids have the same coordinates of previous centroids
+				if nump.sum(distance.euclidean(curr,original_centroid)/original_centroid) > self.approx_error: #verifying if current centroids have the same coordinates of previous centroids
 					end = False #if there is no variation from previous centroids (centroids don't change their positions more than approx_error), set end to false
 			if end:
 				print("Final iteration: "+str(it+1)) #this statement print the final iteration if the algorithm has converged before the max number of iterations
@@ -70,46 +72,66 @@ class K_Means_Alg: 	#class K-Means_Alg define attributes and methods to perform 
 				print("Final iteration: "+str(it+1)) #this statement print the final iteration if the algorithm reach the max number of iterations
 				print("Max iterations reached. Needs more iterations to converge")
 
-def main(path,n=3,approx_error=0.0000, iterations=500): #set arguments(n, approx_error, iterations) to default value if they're no passed
-	
-	with open(path, "r") as f:
-		reader = csv.reader(f)
-		i = next(reader) #i is an array containing the headers of the csv file
-	
-	data = pand.read_csv(path) #read the data source from the path argument
-	if (len(i) == 1):  #if i == 1, csv file cointains monodimensional points, so we add V2 column with 0 values to all points
-		data['V2']= 0.00
-	print(data)
-	np_data = data.values #returns a numpy array
-	
-	km = K_Means_Alg(n, approx_error, iterations) #instantiate km of K_Means_Alg class
-	km.exe(np_data) # perform the algorithm
-
-	# Plotting the results
-	print("Start Plotting..")
-	colors = 30*["b", "r", "g", "c", "m", "y", "k", "#ff8300", "#777777", "#00ff04",  "#840028", "#45ff88", "#00146e", "#7d9300", "#ff6060", "#ffb600"] #every cluster has its color
-	subplt = pyplt.subplot()
-	pyplt.title('K-Means Clusters Results - Approx error : ' + str(approx_error)) 
-
-
-	for centroid in km.centroids:
-		color = colors[centroid] #assign colors to centroid for plotting
-		subplt.scatter(km.centroids[centroid][0], km.centroids[centroid][1], c = color , s = 80, marker= "X", label='Cluster '+str(centroid)) #plot centroids
-
-	for pointclass in km.clusters:
-		color = colors[pointclass] #assign colors to clusters for plotting
-		for point in km.clusters[pointclass]:
-			subplt.scatter(point[0], point[1], color = color,s = 30, alpha= 0.4) #plot clusters
-
-	chartBox = subplt.get_position()
-	subplt.set_position([chartBox.x0, chartBox.y0, chartBox.width*0.85, chartBox.height]) #set the new subplot position based on its original position
-	subplt.legend(loc="upper left", bbox_to_anchor=(1.0, 1.0), shadow=True) #add legend
-	pyplt.savefig('../output/cluster.png') #save a result png
-	pyplt.show()
-
-if __name__ == "__main__":
+def main():
+	# interaction with the user to define the execution parameters
 	cluster_size = int(input("Enter cluster size : "))
 	appr_factor = float(input("Enter approximation factor 0 < appr_factor < 1 : "))
 	cicles = int(input("Enter MAX iterations number executable : "))
 	input_file = input("Enter input file name ( .csv ) : ")
-	main(path='../dataset/'+ input_file, n=cluster_size, approx_error=appr_factor, iterations=cicles) #set arguments for main function (path, n, approx_error, iterations)
+	show = input("Do you want to visualize the result? ( yes or no ) : ")
+
+	path='../dataset/'+input_file #set the path for the input dataset
+
+	with open(path, "r") as f:
+		reader = csv.reader(f)
+		i = next(reader) #'i' is an array containing the headers of the csv file; is useful later for plotting different dataset (monodimensional o bidimensional)
+	
+	data = pand.read_csv(path) #read the data source from the path argument
+	np_data = data.values #returns a numpy array
+	
+	km = K_Means_Alg(cluster_size, appr_factor, cicles) #instantiate km of K_Means_Alg class
+	km.exe(np_data) # perform the algorithm
+
+	# Plotting the results
+	print("Start Plotting..")
+	colors = ["b", "r", "g", "c", "m", "y", "k"] #every cluster has its color. The firsts 7 colors for the firsts 7 clusters are fixed: blue, red, green, ciano, magenta, yellow and black
+
+	#this code block is used to assign different colors dinamically for each clusters fìafter the 7th cluster. 
+	l = len(km.centroids)
+	if l > len(colors):
+		l2 = l - len(colors)
+		for x in range(l2): #l2 is the number of the clusters after the 7th cluster
+			randint = rd.randint(1048576,16777215)
+			rcolor = hex(randint) #hex() convert the randint value in hexadecimal. The generated hexadecimal value is in the range 100000 to FFFFFF
+			colors.append('#'+str(rcolor[2:])) #append the color to the colors list
+
+	
+	subplt = pyplt.subplot()
+	pyplt.title('K-Means Clusters Results - Approx error : ' + str(appr_factor)) 
+
+
+	for centroid in km.centroids:
+		color = colors[centroid] #assign colors to centroid for plotting
+		if(len(i) == 1 ):
+			y = 0
+		else : y = km.centroids[centroid][1]
+		subplt.scatter(km.centroids[centroid][0], y,  c = color , s = 80, marker= "X", label='Cluster '+str(centroid)) #plot centroids. If len(i)==1, the point has one coordinate, so set y=0
+
+	for pointclass in km.clusters:
+		color = colors[pointclass] #assign colors to clusters for plotting
+		for point in km.clusters[pointclass]:
+			if(len(i) == 1 ): #setting the y coordinate for mono or bi dimenisional version 
+				y = 0
+			else : y = point[1]
+			subplt.scatter(point[0], y, color = color,s = 30, alpha= 0.4) #plot clusters
+
+	chartBox = subplt.get_position()
+	subplt.set_position([chartBox.x0, chartBox.y0, chartBox.width*0.85, chartBox.height]) #set the new subplot position based on its original position
+	subplt.legend(loc="upper left", bbox_to_anchor=(1.0, 1.0), shadow=True) #add legend
+	pyplt.savefig('../output/cluster.pdf', bbox_inches='tight') #save a result png
+	print("Result saved in the Output directory")
+	if(show == 'yes'):
+		pyplt.show()
+
+if __name__ == "__main__":
+	main()
